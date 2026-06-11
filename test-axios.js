0e75c73d61dac2abb208092ad2cc9bf95eaf0ca9
Alamgir Islam <alamgirislam009@gmail.com>
2026-06-11 19:09:42 +0530

    latest code with minor update of unpaidbill

--- test-axios.js
+++
@@ -1,8 +0,0 @@
-const axios = require('axios');
-
-const apiClient = axios.create({
-    baseURL: 'http://localhost:8080/api/v1'
-});
-
-console.log(apiClient.getUri({ url: '/meals' }));
-console.log(apiClient.getUri({ url: 'meals' }));
